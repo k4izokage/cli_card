e3497c217449fe39a91a2c81ff71cb67fb0148c9
Kaizokage <kaizokage@duck.com>
2024-07-29 02:45:47 +0700

    publish v2.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# cli personal card
+[![npm version](https://badge.fury.io/js/k4izokage.svg)](https://badge.fury.io/js/k4izokage)
+
+My `npx card` to connect with me directly via console or terminal
+
+## How to use it?
+1. Open your terminal.
+2. Type this command and hit enter 👇.
+```
+npx k4izokage
+```
+3. You will see my personal card in your terminal.
+
+## Screenshot
+<div align="center">
+    <img src="./card.webp" alt="K4izokage's NPX Card" />
+</div>
+
+## Credits
+[Salma Alam-Naylor's blog](https://whitep4nth3r.com/blog/build-a-business-card-cli-tool/)
--- index.mjs
+++ index.mjs
@@ -1,24 +1,7 @@
 #!/usr/bin/env node
 
-"use strict";
-
 import boxen from "boxen";
 import chalk from "chalk";
-import clear from "clear";
-
-clear();
-
-const boxenOptions = {
-    margin: 1,
-    padding: 1,
-    width: 55,
-    float: 'center',
-    textAlignment: "left",
-    title: "k4izokage's business card",
-    titleAlignment: "center",
-    borderStyle: "single",
-    borderColor: "green",
-};
 
 const intro = `${chalk.bold("Hi, I'm")} ${chalk.yellow.italic("k4izokage")}, ${chalk.bold("a developer from Vietnam.\n\n")} ${chalk.italic("Let's connect for exciting possibilities.\n Open to anything under the sun, I will try my best to get back to you!\n\n")}`;
 
@@ -32,22 +15,26 @@ const links = [
         url: chalk.gray("https://github.com/") + chalk.white("k4izokage"),
     },
     {
-        name: chalk.white.bold("X (Twitter):"),
-        url: chalk.gray("https://x.com/") + chalk.hex("#1C9BE9")("k4izokage"),
+        name: chalk.white.bold("Telegram:"),
+        url: chalk.gray("https://t.me/") + chalk.hex("#0A63BC")("k4izokage"),
     },
     {
-        name: chalk.white.bold("Reddit:"),
-        url: chalk.gray("https://reddit.com/user/") + chalk.yellow("k4izokage"),
+        name: chalk.white.bold("X (Twitter):"),
+        url: chalk.gray("https://x.com/") + chalk.hex("#1C9BE9")("k4izokage"),
     },
     {
         name: chalk.white.bold("Youtube:"),
         url: chalk.gray("https://youtube.com/") + chalk.red("@k4izokage"),
     },
     {
-        name: chalk.white.bold("LinkedIn:"),
-        url: chalk.gray("https://linkedin.com/in/") + chalk.hex("#0A63BC")("tran-tien-dat"),
+        name: chalk.white.bold("Reddit:"),
+        url: chalk.gray("https://reddit.com/user/") + chalk.yellow("k4izokage"),
     },
+
+
 ];
+
+const linkList = links.map((link) => `${link.name} ${link.url}`).join("\n");
 
 const tip = [
     `\n\n\n(Tip: Try ${chalk.cyanBright.bold(
@@ -55,6 +42,16 @@ const tip = [
     )} on the links above)`
 ]
 
-const linkList = links.map((link) => `${link.name} ${link.url}`).join("\n");
+const boxenOptions = {
+    margin: 1,
+    padding: 1,
+    width: 55,
+    float: 'center',
+    textAlignment: "left",
+    title: "k4izokage's personal card",
+    titleAlignment: "center",
+    borderStyle: "single",
+    borderColor: "green",
+};
 
 console.log(boxen(intro + linkList + tip, boxenOptions));
